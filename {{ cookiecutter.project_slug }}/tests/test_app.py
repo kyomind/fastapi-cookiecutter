d2a44b0d325a10b93c146ca96e1db0f1d668522f
kyo <odinxp@gmail.com>
2025-07-31 17:26:24 +0800

    Add basic unit test for root endpoint

--- {{ cookiecutter.project_slug }}/tests/test_app.py
+++ {{ cookiecutter.project_slug }}/tests/test_app.py
@@ -1,1 +1,18 @@
 """Basic unit test for FastAPI app."""
+
+from fastapi.testclient import TestClient
+
+
+def test_root_endpoint(client: TestClient) -> None:
+    """
+    Test the root endpoint.
+
+    Args:
+        client: FastAPI test client fixture
+
+    Returns:
+        None
+    """
+    response = client.get("/")
+
+    assert response.status_code == 200
